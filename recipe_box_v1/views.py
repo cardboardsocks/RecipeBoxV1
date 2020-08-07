eec5596a09c0d4c0d92ffed7bdf53dc1105f9df5
sohail aslam <sohailaslam0707@gmail.com>
2020-08-07 14:14:33 -0400

    ready for grading

--- recipe_box_v1/views.py
+++ recipe_box_v1/views.py
@@ -5,15 +5,19 @@ def index(request):
     return render(request,'index.html',{'all_recipes':Recipe.objects.all()})
 
 def recipe_detail(request,recipe_id):
-    _recipe_detail = Recipe.objects.filter(id=recipe_id )
+    _recipe_detail = Recipe.objects.filter(id=recipe_id).first()
     context = {
         "recipe_detail":_recipe_detail
     }
     return render(request,'recipe_detail.html',context)
 
 def author_detail(request,author_id):
-    author_detail =Recipe.objects.filter(id=author_id)
+    _author_detail =Recipe.objects.filter(author__id= author_id)
+    _author_name =Author.objects.get(id= author_id)
+  
+    
     context = {
-        "author_detail":author_detail
+        "author_detail":_author_detail,
+        "author_name":_author_name
     }
     return render(request,'author_detail.html',context)
